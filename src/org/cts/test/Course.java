4d5a6a53146a54cc688470e91287c21780c85d3a
vetrikrrish <vetrikrrish@gmail.com>
2019-03-19 19:48:48 +0530

    file added

---
+++ src/org/cts/test/Course.java
@@ -0,0 +1,23 @@
+package org.cts.test;
+
+public class Course {
+	public void javaCourse() {
+		System.out.println("java course started");
+	}
+		public void oracleCourse() {
+			System.out.println("oracle started");
+			
+		}
+	public static void main(String[] args) {
+		Course js =new Course();
+		js .javaCourse ();
+		js  .oracleCourse();
+	
+		
+	}
+		
+	}	
+	
+	
+
+	
---
+++ src/org/cts/test/Datatype.java
@@ -0,0 +1,24 @@
+package org.cts.test;
+public class Datatype{
+public static void main(String[] args) {
+	byte b=12;
+	System.out.println(b);
+	short s=1234;
+	System.out.println(s);
+	int ih=123456789;
+	System.out.println(ih);
+	long l=123456789;
+	System.out.println(l);
+	char c='+';
+	System.out.println(c);
+	String g="qhwhiu23345@##$%";
+	System.out.println(g);
+	
+}
+	
+
+		
+	}
+	
+
+
